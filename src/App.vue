b26b6a9b2df316474e209dfbe633d16494fe0f2e
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-12-12 19:04:33 -0300

    inicio

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav-bar />
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<style>
+@import "@/assets/css/main.css";
+</style>
