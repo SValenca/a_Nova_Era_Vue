db4cc2b31a56013fed82252bc78501cbee68ea7c
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-12-12 21:43:41 -0300

    login e person

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 import HomeView from "../views/HomeView.vue"
-
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/LoginView.vue"),
   },
   {
     path: "/about",
@@ -17,10 +21,8 @@ const routes: Array<RouteRecordRaw> = [
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ]
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
-
 export default router
